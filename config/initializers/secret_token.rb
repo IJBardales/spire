bba87d472a438abde27c17ecfdebb292453f13d2
Ingrid <olmccb@hotmail.com>
2015-11-12 22:09:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inspire::Application.config.secret_key_base = '63626b63b6bac2ee1283b8451d361dff01db44172f2eb0955d9c961836030f154dffeb7547a3fcafc11cc2cdafc5d6278dc36b106fb0f1063a841c5b1c04d214'
